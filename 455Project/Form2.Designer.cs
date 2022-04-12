6c7a1506a25df2cb9dc73dacde56a19a2c3de954
noah boushee <noah.boushee@outlook.com>
2022-04-12 16:05:26 -0500

    Added Patient and Staff forms

---
+++ 455Project/Form2.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace _455Project
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogOutButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LogOutButton
+            // 
+            this.LogOutButton.Location = new System.Drawing.Point(713, 13);
+            this.LogOutButton.Name = "LogOutButton";
+            this.LogOutButton.Size = new System.Drawing.Size(75, 23);
+            this.LogOutButton.TabIndex = 0;
+            this.LogOutButton.Text = "Log Out";
+            this.LogOutButton.UseVisualStyleBackColor = true;
+            this.LogOutButton.Click += new System.EventHandler(this.LogOutButton_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LogOutButton);
+            this.Name = "Form2";
+            this.Text = "Patient View";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LogOutButton;
+    }
+}
---
+++ 455Project/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace _455Project
+{
+    public partial class Form2 : Form
+    {
+        public SQLiteConnection connection;
+        public Form2()
+        {
+            connection = new SQLiteConnection("Data Source=455DB.db");
+            connection.Open();
+            InitializeComponent();
+        }
+
+        private void LogOutButton_Click(object sender, EventArgs e)
+        {
+            connection.Close();
+            this.Close();
+        }
+    }
+}
